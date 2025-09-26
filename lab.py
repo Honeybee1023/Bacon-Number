8c1d9f0f40a3ea5c856f246e2b4cf888ce4871ba
Honeybee1023 <93350084+Honeybee1023@users.noreply.github.com>
2025-09-26 06:30:58 -0700

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,212 @@
+"""
+6.101 Lab:
+Bacon Number
+"""
+
+#!/usr/bin/env python3
+
+import pickle
+# import typing # optional import
+# import pprint # optional import
+
+# NO ADDITIONAL IMPORTS ALLOWED!
+
+#HELPER FUNCTIONS
+def name_to_ID(name):
+    """
+    Takes in name as string, return ID number of actor with that name as int
+    """
+    with open("resources/names.pickle", "rb") as f:
+        namesdb = pickle.load(f)
+
+    return namesdb[name]
+
+def ID_to_name(ID):
+    """
+    Takes in ID of actor as int, return name of actor with that ID as string
+    """
+    with open("resources/names.pickle", "rb") as f:
+        namesdb = pickle.load(f)
+
+    for key, val in namesdb.items():
+        if val == ID:
+            return(key)
+    
+
+#LAB FUNCTIONS
+
+def transform_data_old(raw_data):
+    """
+    Assume input is list of tuples (Actor ID 1, Actor ID 2, Movie ID)
+
+    We will return data structure of a dictionary where key is a tuple (Actor 1 ID, Actor 2 ID) and value is movie ID
+
+    Reasoning:
+        We will often need to check if two actors are in movie together. So we will often call "if (actor 1, actor 2) in database"
+        This means either set or dict work well, because they are the only 2 data structures we hace learned so far that have
+        constant time access of some form.
+
+        Between these, dict seems better because we may need to access values at some point.
+
+        We must have the two actor IDs as keys of the dictionary for constant time access. Then we will have movie ID as value.
+    """
+    transformed_data = {}
+
+    for data in raw_data:
+        transformed_data[(data[0], data[1])] = data[2]
+    
+    return transformed_data
+
+def transform_data(raw_data):
+    """
+    Assume input is list of tuples (Actor ID 1, Actor ID 2, Movie ID)
+
+    We will return data structure of a dictionary where key is each actor ID, value is set of actors who have directly
+    acted with that actor
+
+    This makes it easy to access neighbors, which is an operation we use a lot later
+    """
+    transformed_data = {}
+
+    #initializing empty set for all actors
+    for data in raw_data:
+        transformed_data[data[0]] = set()
+        transformed_data[data[1]] = set()
+
+    #creating enighbor mapping
+    for data in raw_data:
+        transformed_data[data[0]].add(data[1])
+        transformed_data[data[1]].add(data[0])
+    
+    return transformed_data
+
+
+def acted_together(neighbor_map, actor_id_1, actor_id_2):
+    if actor_id_1 not in neighbor_map.keys() or actor_id_2 not in neighbor_map.keys():
+        return False
+    
+    return (actor_id_2 in neighbor_map[actor_id_1])
+
+def acted_with(neighbor_map, actor_1):
+    """
+    Return a set of all actors who have acted with the given actor
+    """
+    return (neighbor_map[actor_1])
+
+def actors_with_bacon_number(neighbor_map, num):
+    """
+    We will loop through past nums and go layer by layer, and use dict to store past results
+    Note: Do NOT use recursion here because it will lead to repeated calls and O(2^n) time complexity
+    """
+    if num == 0:
+        return {4724}
+
+    else:
+        past_recursions = {0: {4724}}
+        
+        #Make a set of all actors who have lower bacon number already
+        visited = {4724}
+        for n in range(1, num+1):
+            past_recursions[n] = set()
+            for last_actor in past_recursions[n-1]:
+                for actor in acted_with(neighbor_map, last_actor) - visited:
+                    past_recursions[n].add(actor)
+                    visited.add(actor)
+            if len(past_recursions[n]) == 0:
+                return set()
+
+        return past_recursions[num]
+
+def bacon_path(transformed_data, actor_id):
+    if actor_id == 4724:
+        return [4724]
+
+    else:
+        visited = set()
+        agenda = [(4724,)]
+        goal = False
+
+        while not goal:
+            current = agenda.pop(0)
+            visited.add(current)
+
+            for neighbor in acted_with(transformed_data, current[-1]):
+                #print(current[-1], " has neighbor", neighbor)
+                if neighbor == actor_id:
+                    path = list(current) + [neighbor]
+                    return path
+                else:
+                    new_path = current + (neighbor,)
+                    if new_path not in visited:
+                        agenda.append(new_path)
+
+            if agenda == []:
+                return None
+
+
+def actor_to_actor_path(transformed_data, actor_id_1, actor_id_2):
+    raise NotImplementedError("Implement me!")
+
+
+def actor_path(transformed_data, actor_id_1, goal_test_function):
+    raise NotImplementedError("Implement me!")
+
+
+def actors_connecting_films(transformed_data, film1, film2):
+    raise NotImplementedError("Implement me!")
+
+
+if __name__ == "__main__":
+    with open("resources/small.pickle", "rb") as f:
+        smalldb = pickle.load(f)
+
+    with open("resources/names.pickle", "rb") as f:
+        namesdb = pickle.load(f)
+
+    transformed_smalldb = transform_data(smalldb)
+
+    with open("resources/tiny.pickle", "rb") as f:
+        tinydb = pickle.load(f)
+
+    transformed_tinydb = transform_data(tinydb)
+
+    with open("resources/large.pickle", "rb") as f:
+        largedb = pickle.load(f)
+
+    transformed_largedb = transform_data(largedb)
+
+    #print(bacon_path(transformed_tinydb, 1640))
+
+    print(bacon_path(transformed_smalldb, ))
+    
+    #print(bacon_path(transformed_largedb, name_to_ID("Cecile Arnold")))
+
+    #print(actors_with_bacon_number(transformed_largedb, 6))
+
+    #{1367972, 1338716, 1345461, 1345462}
+    
+    #print(ID_to_name(1367972), ID_to_name(1338716), ID_to_name(1345461), ID_to_name(1345462))
+
+    #print(actors_with_bacon_number(transformed_tinydb, 1))
+
+    #print(tinydb)
+    #print(name_to_ID("Kevin Bacon"))
+
+    #print(acted_together(transformed_smalldb, name_to_ID("Robert Viharo"), name_to_ID("Patrick Gorman")))
+    #print(acted_together(transformed_smalldb, name_to_ID("Sten Hellstrom"), name_to_ID("Johan Akerblom")))
+
+    #print(type(namesdb))
+    #print(namesdb)
+
+    #print(namesdb["Anthony J. Baker"])
+
+    #for key, val in namesdb.items():
+    #    if val == 65776:
+    #        print(key)
+
+    
+        
+    # additional code here will be run only when lab.py is invoked directly
+    # (not when imported from test.py), so this is a good place to put code
+    # used, for example, to generate the results for the online questions.
+    pass
